8ca3a3964d2e0f803700e189d2709019f2b02e2f
Index Int <vlad.z.4096@gmail.com>
2014-08-23 00:03:51 +0400

    Refactor sodium-tool

--- src/sodium-tool/Main.hs
+++ src/sodium-tool/Main.hs
@@ -5,14 +5,10 @@ import System.Exit
 import qualified Sodium
 
 main = getArgs >>= \case
-	[filename1, filename2] -> processFiles filename1 filename2
-	_ -> do
-		putStrLn "usage: sodium filename.pas filename.hs"
-		exitWith $ ExitFailure 1
+    [filename1, filename2] -> processFiles filename1 filename2
+    _ -> do putStrLn "usage: sodium filename.pas filename.hs"
+            exitWith $ ExitFailure 1
 
 processFiles :: String -> String -> IO ()
-processFiles filename1 filename2 = do
-	source <- readFile filename1
-	let dest = Sodium.translate source
-	putStrLn dest
-	writeFile filename2 dest
+processFiles filename1 filename2
+    = readFile filename1 >>= writeFile filename2 . Sodium.translate
