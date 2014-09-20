6f120237c5e9ce85400d7093aa9782f076e45b02
Index Int <vlad.z.4096@gmail.com>
2014-09-20 16:55:21 +0400

    Side-effects in conditions of if-statements

--- src/Sodium/Nucleus/Side.hs
+++ src/Sodium/Nucleus/Side.hs
@@ -1,28 +1,35 @@
 module Sodium.Nucleus.Side (side) where
 
-import Control.Lens (over)
+import Control.Lens
 import Control.Monad.Writer
+import Control.Applicative
+import Data.Either
 import qualified Data.Map as M
 import Sodium.Nucleus.Program.Scalar
 import Sodium.Nucleus.Recmap.Scalar
 import Data.Stack
 
+type VarDecl = (Name, ClType)
+
 side :: Program -> Program
-side = over recmapped sideStatement
+side = flip evalStack (map NameGen [0..]) . recmapped sideStatement
 
-sideStatement :: Statement -> Statement
-sideStatement (Assign name expr) = BodyStatement (sideAssign name expr)
-sideStatement statement = statement
+sideStatement :: Statement -> Stack Name Statement
+sideStatement = \case
+    Assign name expr -> BodyStatement <$> sideAssign name expr
+    MultiIfStatement multiIfBranch -> BodyStatement <$> sideMultiIf multiIfBranch
+    statement -> return statement
 
-sideAssign :: Name -> Expression -> Body
-sideAssign name expr = Body (M.fromList vardecls) statements where
-    statements = sidecalls ++ [SideCall name OpId [e]]
-    (e, xs) = evalStack (runWriterT (sideExpression expr)) (map NameGen [0..])
-    (vardecls, sidecalls) = unzip xs
+sideAssign :: Name -> Expression -> Stack Name Body
+sideAssign name expr = do
+    (e, xs) <- runWriterT (sideExpression expr)
+    let (vardecls, sidecalls) = partitionEithers xs
+    let statements = sidecalls ++ [SideCall name OpId [e]]
+    return $ Body (M.fromList vardecls) statements
 
 sideExpression
     :: Expression
-    -> WriterT [((Name, ClType), Statement)] (Stack Name) Expression
+    -> WriterT [Either VarDecl Statement] (Stack Name) Expression
 sideExpression = \case
     Access name -> return (Access name)
     Primary lit -> return (Primary lit)
@@ -30,5 +37,25 @@ sideExpression = \case
         eArgs <- mapM sideExpression args
         name <- pop
         let vardecl = (name, ClVoid) -- TODO: the real type
-        tell [(vardecl, SideCall name op eArgs)]
+        tell [Left vardecl]
+        tell [Right $ SideCall name op eArgs]
         return (Access name)
+
+sideMultiIf :: MultiIfBranch -> Stack Name Body
+sideMultiIf multiIfBranch = do
+    (leafs, xs) <- runWriterT (sideMultiIfLeafs $ view multiIfLeafs multiIfBranch)
+    let (vardecls, assigns) = partitionEithers xs
+    let statements = assigns ++ [MultiIfStatement $ set multiIfLeafs leafs multiIfBranch]
+    return $ Body (M.fromList vardecls) statements
+
+sideMultiIfLeafs
+    :: [(Expression, Body)]
+    -> WriterT [Either VarDecl Statement] (Stack Name) [(Expression, Body)]
+sideMultiIfLeafs leafs = do
+    forM leafs  $ \(expr, body) -> do
+        name <- pop
+        let vardecl = (name, ClVoid) -- TODO: the real type
+        tell [Left vardecl]
+        assign <- lift (sideAssign name expr)
+        tell [Right $ BodyStatement assign]
+        return (Access name, body)
