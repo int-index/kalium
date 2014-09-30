6ebecac53109ceff756c7728d828188d4e133d12
Index Int <vlad.z.4096@gmail.com>
2014-09-30 12:50:21 +0400

    Test comments parsing

--- testing/tests/basic-002/program.pas
+++ testing/tests/basic-002/program.pas
@@ -1,2 +1,23 @@
 BEGIN
-END.
+    {
+      Haskell is an advanced
+      purely-functional programming
+      language.  
+    }
+
+    {----------------------------------------------------}
+    (* An open-source product of more than twenty years *)
+    {  of cutting-edge research, it allows rapid        *)
+    (* development of robust, concise, correct software. }
+    (*--------------------------------------------------*)
+
+    (*
+       Haskell makes it easier to produce flexible,
+       maintainable, high-quality software.
+    *)
+END.%%%
+
+Pascal is an influential imperative and procedural
+programming language, designed as a small and efficient
+language to encourage good programming practices usign
+structured programming.
