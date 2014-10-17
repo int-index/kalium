49b43f376c3901244811566197f79e246af8058a
Index Int <vlad.z.4096@gmail.com>
2014-10-17 09:31:32 +0400

    Refactor the Vectorize module

--- src/Sodium/Nucleus/Vectorize.hs
+++ src/Sodium/Nucleus/Vectorize.hs
@@ -1,5 +1,7 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE ViewPatterns #-}
-module Sodium.Nucleus.Vectorize (vectorize, Error(..)) where
+module Sodium.Nucleus.Vectorize (vectorize, Error(..), degroup) where
 
 import Data.List
 import Control.Applicative
@@ -11,8 +13,6 @@ import qualified Data.Map as M
 import Sodium.Nucleus.Program.Scalar
 import qualified Sodium.Nucleus.Program.Vector as Vec
 
-import Debug.Trace
-
 data Error
     = NoAccess Name Vec.Indices
     | NoFunction Name
@@ -20,11 +20,10 @@ data Error
     | NoReference
     deriving (Show)
 
-type E = Except Error
-type R m a = ReaderT Vec.Indices m a
-type S m a = StateT  Vec.Indices m a
+type E m = (Applicative m, MonadError Error m)
+type V t m = (MonadTrans t, MonadReader Vec.Indices (t m), E m, E (t m))
 
-vectorize :: Program Atom -> E Vec.Program
+vectorize :: E m => Program Atom -> m Vec.Program
 vectorize program = do
     let funcSigs = program ^.. programFuncs . traversed . funcSig
     vecFuncs <- mapM (vectorizeFunc funcSigs) (program ^. programFuncs)
@@ -36,7 +35,7 @@ references params paramTypes = unzip $ do
     guard (by == ByReference)
     return (name, ty)
 
-vectorizeFunc :: [FuncSig] -> Func Atom -> E Vec.Func
+vectorizeFunc :: E m => [FuncSig] -> Func Atom -> m Vec.Func
 vectorizeFunc funcSigs func = do
     let r = vectorizeScope funcSigs (func ^. funcScope)
     (_, vecBodyGen)
@@ -54,66 +53,56 @@ vectorizeFunc funcSigs func = do
 patTuple = Vec.PTuple . map (uncurry Vec.PAccess)
 expTuple = Vec.Tuple  . map (uncurry Vec.Access)
 
-vectorizeScope :: [FuncSig] -> Scope Atom -> R E ([Name], [Atom] -> E Vec.Body)
+vectorizeScope :: V t m => [FuncSig] -> Scope Atom -> t m ([Name], [Atom] -> m Vec.Body)
 vectorizeScope funcSigs scope = do
     let vars = scope ^. scopeVars
-    closure <- ask
-    lift $ do
-        let isLocal = flip elem (M.keys vars)
-        let indices = initIndices Vec.Uninitialized vars `M.union` closure
-        (vecStatement, indices')
-                 <- (`runStateT` indices) $ do
-                        (changed, vecStatement) <- readerToState (vectorizeStatement funcSigs (scope ^. scopeStatement))
-                        changed' <- mapM registerIndexUpdate changed
-                        return (changed', vecStatement)
-        let changed
-                = M.keys
-                $ M.filterWithKey ((&&) . not . isLocal)
-                $ M.intersectionWith (/=)
-                indices indices'
+    local (initIndices Vec.Uninitialized vars `M.union`) $ do
+        (boundIndices, vecBind) <- do
+            (changed, vecStatement) <- vectorizeStatement funcSigs (scope ^. scopeStatement)
+            changed' <- mapM (naming indexUpdate) changed
+            return (changed', Vec.Bind (patTuple changed') vecStatement)
+        let changed = filter (`M.notMember` vars) (map fst boundIndices)
+        indices' <- asks (M.fromList boundIndices `M.union`)
         let vecBodyGen results
-                = Vec.Body vars
-                [(\(indices, expr) -> Vec.Bind (patTuple indices) expr) vecStatement]
+                = Vec.Body vars [vecBind]
                 <$> runReaderT (Vec.Tuple <$> mapM vectorizeAtom results) indices'
         return (changed, vecBodyGen)
 
+degroup funcSigs statements act = do
+    indices <- ask
+    flip evalStateT indices $ do
+       vecBinds <- forM statements $ \statement -> do
+          (changed, vecStatement) <- readerToState (vectorizeStatement funcSigs statement)
+          changed' <- forM changed $ naming $ \name -> do
+                index <- readerToState (indexUpdate name)
+                modify $ M.insert name index
+                return index
+          return $ Vec.Bind (patTuple changed') vecStatement
+       readerToState (act indices vecBinds)
 
-vectorizeStatement' :: [FuncSig] -> Statement Atom -> S E ([(Name, Vec.Index)], Vec.Statement)
-vectorizeStatement' funcSigs statement
-    = do
-        (changed, vecStatement) <- readerToState (vectorizeStatement funcSigs statement)
-        changed' <- mapM registerIndexUpdate changed
-        return (changed', vecStatement)
+diff :: (Ord k, Eq v) => M.Map k v -> M.Map k v -> [k]
+diff m1 m2 = M.keys $ M.filter id $ M.intersectionWith (/=) m1 m2
 
-vectorizeStatement :: [FuncSig] -> Statement Atom -> R E ([Name], Vec.Statement)
+vectorizeStatement :: V t m => [FuncSig] -> Statement Atom -> t m ([Name], Vec.Statement)
 vectorizeStatement funcSigs = \case
     Group statements -> do
-        indices <- trace "group!" ask
-        lift $ do
-            (vecStatements, indices')
-                    <- flip runStateT indices
-                     $ mapM (vectorizeStatement' funcSigs) statements
-            let changed
-                    = M.keys
-                    $ M.filter id
-                    $ M.intersectionWith (/=)
-                    indices indices'
-            vecBody
-                    <- Vec.Body M.empty
-                    (map (\(indices, expr) -> Vec.Bind (patTuple indices) expr) vecStatements)
-                    <$> runReaderT (Vec.Tuple <$> mapM vectorizeAtom (map Access changed)) indices'
-            return (changed, Vec.BodyStatement vecBody)
-    ScopeStatement scope -> over _2 Vec.BodyStatement <$> do
+        degroup funcSigs statements
+            $ \indices vecBinds -> do
+                changed <- asks (diff indices)
+                results <- Vec.Tuple <$> mapM vectorizeAtom (map Access changed)
+                let vecBody = Vec.Body M.empty vecBinds results
+                return (changed, Vec.BodyStatement vecBody)
+    ScopeStatement scope -> do
         (changed, vecBodyGen) <- vectorizeScope funcSigs scope
         vecBody <- lift $ vecBodyGen (map Access changed)
-        return (changed, vecBody)
+        return (changed, Vec.BodyStatement vecBody)
     Execute (Exec mres name args) -> do
         vecArgs <- mapM vectorizeAtom args
         let byReference (ByReference, _) = \case
               Vec.Access name _ -> Just [name]
               _                 -> Nothing
             byReference (ByValue, _) = const (Just [])
-        funcSig <- lift (lookupFuncSig funcSigs name)
+        funcSig <- lookupFuncSig funcSigs name
         sidenames <- case zipWith byReference (funcSig ^. funcParamTypes) vecArgs
                           & sequence
                      of Nothing -> throwError NoReference
@@ -129,17 +118,17 @@ vectorizeStatement funcSigs = \case
               | impure    = Vec.Execute name vecArgs
               | otherwise = Vec.Assign (Vec.Call name vecArgs)
         return $ (resnames ++ sidenames, vecExecute)
-    ForStatement forCycle -> over _2 Vec.ForStatement <$> do
+    ForStatement forCycle -> do
         vecRange <- vectorizeAtom (forCycle ^. forRange)
         let np f = f (forCycle ^. forName) Vec.Immutable
         (changed, vecStatement)
             <- local (np M.insert)
              $ vectorizeStatement funcSigs (forCycle ^. forStatement)
-        argIndices <- closedIndices changed
+        argIndices <- mapM (naming lookupIndex) changed
         let vecLambda = Vec.Lambda [patTuple argIndices, np Vec.PAccess] vecStatement
         let vecForCycle = Vec.ForCycle vecLambda (expTuple argIndices) vecRange
-        return (changed, vecForCycle)
-    IfStatement ifb -> over _2 Vec.MultiIfStatement <$> do
+        return (changed, Vec.ForStatement vecForCycle)
+    IfStatement ifb -> do
         vecCond <- vectorizeAtom (ifb ^. ifCond)
         (changedThen, vecBodyThenGen) <- vectorizeScope funcSigs (Scope M.empty (ifb ^. ifThen))
         (changedElse, vecBodyElseGen) <- vectorizeScope funcSigs (Scope M.empty (ifb ^. ifElse))
@@ -151,9 +140,9 @@ vectorizeStatement funcSigs = \case
                 [ (vecCond, Vec.BodyStatement vecBodyThen)
                 , (Vec.Primary (LitBoolean True), Vec.BodyStatement vecBodyElse)
                 ]
-        return $ (changed, vecMultiIf :: Vec.MultiIf Vec.Statement)
+        return $ (changed, Vec.MultiIfStatement vecMultiIf)
 
-vectorizeAtom :: Atom -> R E Vec.Expression
+vectorizeAtom :: V t m => Atom -> t m Vec.Expression
 vectorizeAtom = \case
     Primary a -> return (Vec.Primary a)
     Access name -> Vec.Access name <$> lookupIndex name
@@ -161,33 +150,26 @@ vectorizeAtom = \case
 readerToState :: (Functor m, Monad m) => ReaderT x m a -> StateT x m a
 readerToState reader = StateT $ \x -> (,x) <$> runReaderT reader x
 
-lookupIndex :: Name -> R E Vec.Index
+lookupIndex :: V t m => Name -> t m Vec.Index
 lookupIndex name = do
     indices <- ask
     M.lookup name indices
        & maybe (throwError $ NoAccess name indices) return
 
-lookupFuncSig :: [FuncSig] -> Name -> E FuncSig
+lookupFuncSig :: E m => [FuncSig] -> Name -> m FuncSig
 -- TODO: real signatures for builtin operators
 lookupFuncSig _ (NameOp _) = return $ FuncSig (Name "") [] (TypeUnit)
 lookupFuncSig funcSigs name
     = find (\funcSig -> view funcName funcSig == name) funcSigs
     & maybe (throwError $ NoFunction name) return
 
-registerIndexUpdate :: Name -> S E (Name, Vec.Index)
-registerIndexUpdate name = do
-    index <- readerToState (lookupIndex name >>= indexUpdate)
-    modify $ M.insert name index
-    return (name, index)
-    where indexUpdate = \case
-            Vec.Index n -> return (Vec.Index $ succ n)
-            Vec.Uninitialized -> return (Vec.Index 0)
-            Vec.Immutable -> throwError (UpdateImmutable name)
+indexUpdate :: V t m => Name -> t m Vec.Index
+indexUpdate name = lookupIndex name >>= \case
+    Vec.Index n -> return (Vec.Index $ succ n)
+    Vec.Uninitialized -> return (Vec.Index 0)
+    Vec.Immutable -> throwError (UpdateImmutable name)
 
-closedIndices :: [Name] -> R E [(Name, Vec.Index)]
-closedIndices = mapM $ \name -> do
-    index <- lookupIndex name
-    return (name, index)
+naming op = \name -> (,) name <$> op name
 
 initIndices :: Vec.Index -> M.Map Name a -> Vec.Indices
 initIndices n = M.map (const n)
