ddb475b3bf3eb693310164deaf3bdb12768ca20e
Index Int <vlad.z.4096@gmail.com>
2014-11-18 13:20:04 +0300

    Proper call substitution during valuefication

--- src/Sodium/Nucleus/Scalar/Valueficate.hs
+++ src/Sodium/Nucleus/Scalar/Valueficate.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
 module Sodium.Nucleus.Scalar.Valueficate where
 
 import qualified Data.Map as M
@@ -8,28 +10,48 @@ import Control.Monad
 
 import Sodium.Nucleus.Scalar.Program
 
-instance Functor (Statement pat) where
-    fmap f = \case
-        Follow a1 a2 -> Follow (fmap f a1) (fmap f a2)
-        ScopeStatement a -> ScopeStatement (fmap f a)
-        IfStatement  a -> IfStatement  (fmap f a)
-        ForStatement a -> ForStatement (fmap f a)
-        Execute      a -> Execute      (fmap f a)
+class ValueficateSubstitute a where
+    valueficateSubstitute :: ReferenceInfo -> a Atom -> a Expression
+
+instance ValueficateSubstitute (Exec Pattern) where
+    valueficateSubstitute referenceInfo (Exec ret op args) =
+        let ret' = patTuple (ret:rets)
+            rets = case M.lookup op referenceInfo of
+                Nothing -> []
+                Just currentReferenceInfo -> do
+                    (keep, arg) <- zip currentReferenceInfo args
+                    guard keep
+                    return $ case arg of
+                        Access name -> PAccess name
+                        Primary _ -> error "non-variables by-ref"
+        in Exec ret' op (map Atom args)
+
+instance ValueficateSubstitute (Statement Pattern) where
+    valueficateSubstitute referenceInfo = \case
+        Follow a1 a2 -> Follow (go a1) (go a2)
+        ScopeStatement a -> ScopeStatement (go a)
+        IfStatement  a -> IfStatement  (go a)
+        ForStatement a -> ForStatement (go a)
+        Execute      a -> Execute      (go a)
         Pass -> Pass
+        where go = valueficateSubstitute referenceInfo
 
-instance Functor (obj pat) => Functor (Scope vars obj pat) where
-    fmap f = scopeElem %~ fmap f
+instance   ValueficateSubstitute (obj pat)
+        => ValueficateSubstitute (Scope vars obj pat) where
+    valueficateSubstitute referenceInfo
+       = scopeElem %~ valueficateSubstitute referenceInfo
 
-instance Functor (If pat) where
-    fmap f (If    ifCond          ifThen          ifElse)
-          = If (f ifCond) (fmap f ifThen) (fmap f ifElse)
+instance ValueficateSubstitute (If Pattern) where
+    valueficateSubstitute referenceInfo
+        (If       ifCond      ifThen      ifElse)
+       = If (Atom ifCond) (go ifThen) (go ifElse)
+       where go = valueficateSubstitute referenceInfo
 
-instance Functor (ForCycle pat) where
-    fmap f (ForCycle forName    forRange          forStatement)
-          = ForCycle forName (f forRange) (fmap f forStatement)
-
-instance Functor (Exec pat) where
-    fmap f = execArgs %~ map f
+instance ValueficateSubstitute (ForCycle Pattern) where
+    valueficateSubstitute referenceInfo
+        (ForCycle forName       forRange      forStatement)
+       = ForCycle forName (Atom forRange) (go forStatement)
+       where go = valueficateSubstitute referenceInfo
 
 valueficate :: Program ByType Pattern Atom -> Program Type Pattern Expression
 valueficate program =
@@ -55,7 +77,7 @@ valueficateFunc referenceInfo name
             guard keep
             return param
 
-          statement' = fmap Atom statement
+          statement' = valueficateSubstitute referenceInfo statement
 
       in Func ty' (Scope params' (Scope vars (Body statement' result')))
 
@@ -64,6 +86,9 @@ typeTuple = foldr1 TypePair
 
 exprTuple :: [Expression] -> Expression
 exprTuple = foldr1 (\x y -> Call (NameOp OpPair) [x, y])
+
+patTuple :: [Pattern] -> Pattern
+patTuple = foldr1 PTuple
 
 type ReferenceInfo = M.Map Name [Bool]
 
