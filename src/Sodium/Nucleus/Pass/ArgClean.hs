470733c8545a4c6315e6de59b7c51229d99ad191
Index Int <vlad.z.4096@gmail.com>
2014-11-28 01:01:01 +0300

    Revive argument cleaning

--- src/Sodium/Nucleus/Pass/ArgClean.hs
+++ src/Sodium/Nucleus/Pass/ArgClean.hs
@@ -1,4 +1,4 @@
-
+{-# LANGUAGE ViewPatterns #-}
 module Sodium.Nucleus.Pass.ArgClean (argClean) where
 
 import Data.Monoid
@@ -14,14 +14,18 @@ argClean :: Program -> Program
 argClean program = p & over recmapped (appEndo f)
     where (p, f) = runWriter (program & (programFuncs . traversed) argCleanFunc)
 
+getDeepCall :: Name1 IndexTag -> Expression -> Maybe [Expression]
+getDeepCall name (Access name1) | name == name1 = Just []
+getDeepCall name (Call expr1 expr2) = (:) expr2 `fmap` getDeepCall name expr1
+getDeepCall _ _ = Nothing
+
 argCleanFunc :: Func -> Writer (Endo Expression) Func
-argCleanFunc = return
-{-
 argCleanFunc func = do
     let mparams = map tag $ func ^. funcLambda . lamPatterns
-    let f (Call op args) | op == Access (func ^. funcSig . funcName)
-            = Call op (catMaybes $ zipWith untag mparams args)
-        f expr = expr
+    let op = func ^. funcSig . funcName
+        f expr | Just (reverse -> args) <- getDeepCall op expr
+               = foldl1 Call (Access op : catMaybes (zipWith untag mparams args))
+               | otherwise = expr
     tell (Endo f)
     func & funcLambda . lamPatterns .~ catMaybes mparams
          & funcSig . funcParamTypes %~ catMaybes . zipWith untag mparams
@@ -31,4 +35,3 @@ argCleanFunc func = do
           tag _ = Nothing
           untag Nothing  _ = Nothing
           untag (Just _) x = Just x
--}
