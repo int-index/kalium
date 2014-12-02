c34ea60d0e65b29a145e41e521e7ad8e605bd610
Index Int <vlad.z.4096@gmail.com>
2014-12-03 02:15:49 +0300

    Revive usage-cleaning

--- src/Sodium/Nucleus/Vector/Match.hs
+++ src/Sodium/Nucleus/Vector/Match.hs
@@ -4,11 +4,30 @@ import Control.Lens
 
 import Sodium.Nucleus.Vector.Program
 import Sodium.Nucleus.Vector.Recmap
+import Sodium.Nucleus.Vector.Name
 
 match :: Program -> Program
-match = over recmapped (bindToApp)
+match = over recmapped (bindToApp . lamClean . lamReduce)
 
 bindToApp :: Expression -> Expression
 bindToApp = \case
     App2 (OpAccess OpBind) (OpAccess OpTaint `App` a) x -> App x a
+    e -> e
+
+cleanUsage :: Mask scope => scope -> Pattern -> Pattern
+cleanUsage a = go where
+    go = \case
+        PUnit -> PWildCard
+        PTuple p1 p2 -> PTuple (go p1) (go p2)
+        PAccess name _ | not (checkRef a name) -> PWildCard
+        p -> p
+
+lamClean :: Expression -> Expression
+lamClean = \case
+    Lambda pat a -> Lambda (cleanUsage a pat) a
+    e -> e
+
+lamReduce :: Expression -> Expression
+lamReduce = \case
+    Lambda PWildCard a `App` _ -> a
     e -> e
