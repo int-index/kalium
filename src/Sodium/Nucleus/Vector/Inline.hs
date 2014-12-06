0c892d867fa9d266e8dd21cb63c5b9c45c236df0
Index Int <vlad.z.4096@gmail.com>
2014-12-07 02:35:28 +0300

    Implement trivial inlining

--- src/Sodium/Nucleus/Vector/Inline.hs
+++ src/Sodium/Nucleus/Vector/Inline.hs
@@ -1,12 +1,29 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Sodium.Nucleus.Vector.Inline where
 
 import Control.Lens
+import Control.Monad.Writer
 
 import Sodium.Nucleus.Vector.Program
 import Sodium.Nucleus.Vector.Recmap
+import Sodium.Nucleus.Vector.Pattern
+import Sodium.Nucleus.Vector.Name
 
 inline :: Program -> Program
 inline = over recmapped inlineExpression
 
 inlineExpression :: Expression -> Expression
-inlineExpression = id
+inlineExpression (Lambda p a `Beta` x) | not excessive, not dangling = b
+    where (b, count) = runWriter (recmapped (inl x p) a)
+          excessive = case x of
+            Access _ -> False
+            OpAccess OpUnit -> False
+            _ -> getSum count > 1
+          dangling = b `mentions` patBound p
+inlineExpression e = e
+
+inl :: MonadWriter (Sum Integer) m => Expression -> Pattern -> Expression -> m Expression
+inl replacement model e | preciseMatch model e = do
+    tell (Sum 1)
+    return replacement
+inl _ _ e = return e
