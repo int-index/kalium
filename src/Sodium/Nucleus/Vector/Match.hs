b97fb8af82c3cbd89e6ba470a9aef02f5ecbabcb
Index Int <vlad.z.4096@gmail.com>
2014-12-11 04:13:11 +0300

    Simple compile-time computations

--- src/Sodium/Nucleus/Vector/Match.hs
+++ src/Sodium/Nucleus/Vector/Match.hs
@@ -9,10 +9,19 @@ import Sodium.Nucleus.Vector.Name
 import Sodium.Nucleus.Vector.Attempt
 
 match :: Program -> Program
-match = over recmapped (matchExpression . lambdaReduce . etaReduce . pairReduce)
+match = over recmapped
+      $ matchExpression
+      . lambdaReduce
+      . etaReduce
+      . pairReduce
+      . foldMatch
+      . booleanCompute
+      . numericCompute
 
 pattern LitZero = Primary (LitInteger 0)
 pattern LitOne  = Primary (LitInteger 1)
+pattern LitTrue = OpAccess OpTrue
+pattern LitFalse = OpAccess OpFalse
 
 matchExpression :: Expression -> Expression
 matchExpression = \case
@@ -36,14 +45,6 @@ matchExpression = \case
 
     AppOp1 OpPutLn (AppOp1 OpShow a) -> AppOp1 OpPrintLn a
 
-    AppOp3 OpFoldTainted (Lambda2 p1 p2 (Taint a)) x1 x2
-        -> Taint (AppOp3 OpFold (Lambda2 p1 p2 a) x1 x2)
-
-    AppOp3 OpFold (OpAccess OpMultiply) LitOne x -> AppOp1 OpProduct x
-    AppOp3 OpFold (OpAccess OpAdd) LitZero x -> AppOp1 OpSum x
-    AppOp3 OpFold (OpAccess OpAnd) (OpAccess OpTrue) x -> AppOp1 OpAnd' x
-    AppOp3 OpFold (OpAccess OpOr) (OpAccess OpFalse) x -> AppOp1 OpOr' x
-
     AppOp3 OpIf xElse xThen cond
         | AppOp1 opElse aElse <- xElse
         , AppOp1 opThen aThen <- xThen
@@ -54,6 +55,75 @@ matchExpression = \case
         -> AppOp1 opElse (AppOp3 OpIf aElse aThen cond)
 
     e -> e
+
+foldMatch :: Expression -> Expression
+foldMatch = \case
+    AppOp3 OpFoldTainted (Lambda2 p1 p2 (Taint a)) x1 x2
+        -> Taint (AppOp3 OpFold (Lambda2 p1 p2 a) x1 x2)
+
+    AppOp3 OpFold (OpAccess OpMultiply) LitOne x -> AppOp1 OpProduct x
+    AppOp3 OpFold (OpAccess OpAdd) LitZero x -> AppOp1 OpSum x
+    AppOp3 OpFold (OpAccess OpAnd) LitTrue x -> AppOp1 OpAnd' x
+    AppOp3 OpFold (OpAccess OpOr) LitFalse x -> AppOp1 OpOr' x
+
+    e -> e
+
+booleanCompute :: Expression -> Expression
+booleanCompute = \case
+
+    AppOp1 OpNot LitTrue  -> LitFalse
+    AppOp1 OpNot LitFalse -> LitTrue
+
+    AppOp2 op x y | x == y, op == OpAnd || op == OpOr -> x
+
+    AppOp2 OpAnd x y | x == AppOp1 OpNot y || AppOp1 OpNot x == y -> LitFalse
+    AppOp2 OpOr  x y | x == AppOp1 OpNot y || AppOp1 OpNot x == y -> LitTrue
+
+    AppOp2 OpAnd (OpAccess OpTrue) a -> a
+    AppOp2 OpAnd a (OpAccess OpTrue) -> a
+    AppOp2 OpOr a (OpAccess OpFalse) -> a
+    AppOp2 OpOr (OpAccess OpFalse) a -> a
+
+    AppOp2 OpAnd x y | x == LitFalse || y == LitFalse -> LitFalse
+    AppOp2 OpOr  x y | x == LitTrue  || y == LitTrue  -> LitTrue
+
+    AppOp3 OpIf xElse _ LitFalse -> xElse
+    AppOp3 OpIf _ xThen LitTrue  -> xThen
+
+    e -> e
+
+numericCompute :: Expression -> Expression
+numericCompute = \case
+
+    AppOp2 op (Primary (LitDouble x)) (Primary (LitDouble y))
+        | op == OpEquals -> if x == y then LitTrue else LitFalse
+        | Just f <- doubleOp2 op -> Primary (LitDouble (f x y))
+
+    AppOp2 op (Primary (LitInteger x)) (Primary (LitInteger y))
+        | op == OpEquals -> if x == y then LitTrue else LitFalse
+        | Just f <- integerOp2 op -> Primary (LitInteger (f x y))
+
+    AppOp1 OpIntToDouble (Primary (LitInteger a))
+        -> Primary (LitDouble (fromIntegral a))
+
+    e -> e
+
+integerOp2 :: Operator -> Maybe (Integer -> Integer -> Integer)
+integerOp2 = \case
+    OpAdd -> return (+)
+    OpSubtract -> return (-)
+    OpMultiply -> return (*)
+    OpDiv -> return div
+    OpMod -> return mod
+    _ -> Nothing
+
+doubleOp2 :: Operator -> Maybe (Rational -> Rational -> Rational)
+doubleOp2 = \case
+    OpAdd -> return (+)
+    OpSubtract -> return (-)
+    OpMultiply -> return (*)
+    OpDivide -> return (/)
+    _ -> Nothing
 
 pairReduce :: Expression -> Expression
 pairReduce = \case
@@ -71,6 +141,7 @@ pairReduce = \case
 -- TODO: typecheck
 isTaintedUnit :: Expression -> Bool
 isTaintedUnit = \case
+    Taint LitUnit -> True
     AppOp1 OpPutLn  _ -> True
     AppOp1 OpPrintLn _ -> True
     AppOp1 OpIgnore _ -> True
