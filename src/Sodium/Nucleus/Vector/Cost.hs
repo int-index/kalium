d6c065569f2581abf4faf07e79c23d6c8e6d4473
Index Int <vlad.z.4096@gmail.com>
2014-12-23 17:51:27 +0300

    Unboxed fields for cost estimation

--- src/Sodium/Nucleus/Vector/Cost.hs
+++ src/Sodium/Nucleus/Vector/Cost.hs
@@ -1,43 +1,42 @@
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE BangPatterns #-}
 module Sodium.Nucleus.Vector.Cost where
 
 import Data.List
 import Data.Ord
-import Data.Monoid
-import Data.Function
 import Control.Applicative
-import Control.Monad.Writer
+import Control.Monad.Writer.Strict
+
+import GHC.Exts (Int(I#), Int#, (+#))
 
 import Sodium.Nucleus.Vector.Program
 import Sodium.Nucleus.Vector.Recmap
 
 data Cost = Cost
-    { _countAccess :: Sum Integer
-    , _countPrimary :: Sum Integer
-    , _countLambda :: Sum Integer
-    , _countBeta :: Sum Integer
+    { _countAccess :: Int#
+    , _countPrimary :: Int#
+    , _countLambda :: Int#
+    , _countBeta :: Int#
     } deriving (Eq)
 
 instance Monoid Cost where
-    mempty = Cost mempty mempty mempty mempty
-    mappend c1 c2 = Cost
-        (on mappend _countAccess  c1 c2)
-        (on mappend _countPrimary c1 c2)
-        (on mappend _countLambda  c1 c2)
-        (on mappend _countBeta    c1 c2)
+    mempty = Cost 0# 0# 0# 0#
+    mappend (Cost a p l b) (Cost a' p' l' b')
+        = Cost (a +# a') (p +# p') (l +# l') (b +# b')
 
 estimate :: Expression -> Cost
 estimate e = execWriter (recmapped w e) where
     w e = e <$ case e of
-        Access   _ -> tell (mempty { _countAccess  = Sum 1 })
-        Primary  _ -> tell (mempty { _countPrimary = Sum 1 })
-        Lambda _ _ -> tell (mempty { _countLambda  = Sum 1 })
-        Beta   _ _ -> tell (mempty { _countBeta    = Sum 1 })
+        Access   _ -> tell (mempty { _countAccess  = 1# })
+        Primary  _ -> tell (mempty { _countPrimary = 1# })
+        Lambda _ _ -> tell (mempty { _countLambda  = 1# })
+        Beta   _ _ -> tell (mempty { _countBeta    = 1# })
 
-estimateSimple :: Expression -> Integer
-estimateSimple e
-    = let Cost{..} = estimate e in getSum
-    $ mconcat [_countAccess, _countPrimary, _countLambda, _countBeta]
+estimateSimple :: Expression -> Int
+estimateSimple e =
+    let !Cost{..} = estimate e
+    in I# (_countAccess +# _countPrimary +# _countLambda +# _countBeta)
 
 lesser :: Expression -> [Expression] -> Expression
 lesser e es = minimumBy (comparing estimateSimple) (e:es)
